e4e664b8dfd5e473567aa4d48a03be9df3086ce3
pnadolny13 <patnadolny@gmail.com>
2023-07-18 17:10:05 -0400

    implement sink to process batch and call API

--- target_hubspot/sinks.py
+++ target_hubspot/sinks.py
@@ -2,42 +2,92 @@
 
 from __future__ import annotations
 
+import csv
+import datetime
+import json
+import os
+import time
+
+from dateutil import parser
+from hubspot import HubSpot
+from singer_sdk.helpers._typing import (
+    DatetimeErrorTreatmentEnum,
+    get_datelike_property_type,
+    handle_invalid_timestamp_in_record
+)
 from singer_sdk.sinks import BatchSink
 
+from target_hubspot.exception import PartialImportException
+
+
+IMPORT_OPERATIONS_LOOKUP = {
+    "CREATE": {"0-1": "CREATE"},
+    "UPDATE": {"0-2": "UPDATE"},
+    "UPSERT": {"0-3": "UPSERT"},
+}
 
 class HubSpotSink(BatchSink):
     """HubSpot target sink class."""
 
     max_size = 10000  # Max records to write in one batch
 
-    def start_batch(self, context: dict) -> None:
-        """Start a batch.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.api_client = HubSpot(access_token=self.config["access_token"])
 
-        Developers may optionally add additional markers to the `context` dict,
-        which is unique to this batch.
+    def _write_csv(self, csv_filename, records):
+        with open(csv_filename, "w") as f:
+            writer = csv.DictWriter(f, fieldnames=self.schema["properties"].keys())
+            writer.writeheader()
+            for record in records:
+                writer.writerow(record)
 
-        Args:
-            context: Stream partition or context dictionary.
-        """
-        # Sample:
-        # ------
-        # batch_key = context["batch_id"]
-        # context["file_path"] = f"{batch_key}.csv"
+    def _validate_column_mapping(self, records):
+        # TODO: raise exception if records and column mapping counts dont match
+        pass
 
-    def process_record(self, record: dict, context: dict) -> None:
-        """Process the record.
+    def _build_request_kwargs(self, csv_filename, import_name):
+        import_request = {
+            "name": import_name,
+            "importOperations": IMPORT_OPERATIONS_LOOKUP.get(self.config["import_operations"]),
+            "dateFormat": self.config["date_format"],
+            "files": [
+            {
+                "fileName": csv_filename,
+                "fileFormat": "CSV",
+                "fileImportPage": {
+                    "hasHeader": True,
+                    "columnMappings": self.config["column_mapping"]
 
-        Developers may optionally read or write additional markers within the
-        passed `context` dict from the current batch.
+                }
+            }
+            ]
+        }
+        filtered_kwargs = {"files": csv_filename}
+        filtered_kwargs['import_request'] = json.dumps(import_request)
+        return filtered_kwargs
 
-        Args:
-            record: Individual record in the stream.
-            context: Stream partition or context dictionary.
-        """
-        # Sample:
-        # ------
-        # with open(context["file_path"], "a") as csvfile:
-        #     csvfile.write(record)
+    def _start_import(self, request_kwargs):
+        response = self.api_client.crm.imports.core_api.create(**request_kwargs)
+        return response.id, response.state
+
+    def _get_import_status(self, import_id):
+        response = self.api_client.crm.imports.core_api.get_by_id(import_id)
+        return response.state, response.metadata
+
+    def _validate_completion_metadata(self, import_id, counters):
+        if counters.get("ERRORS"):
+            raise PartialImportException(f"Import {import_id} Had Errors Importing. See your import history https://knowledge.hubspot.com/crm-setup/view-and-analyze-previous-imports for more information on troubleshooting errors: {counters}")
+        self.logger.info(f"Import {import_id} Completed: {counters}")
+
+    def _import_csv_and_poll_for_status(self, request_kwargs):
+        response = self.api_client.crm.imports.core_api.create(**request_kwargs)
+        import_id, state = self._start_import(request_kwargs)
+        while state not in {"DONE", "FAILED", "CANCELED"}:
+            time.sleep(2)
+            state, metadata = self._get_import_status(import_id)
+            self.logger.debug(f"Import {import_id} Status: {state}")
+        return metadata.counters
 
     def process_batch(self, context: dict) -> None:
         """Write out any prepped records and return once fully written.
@@ -45,7 +95,60 @@ class HubSpotSink(BatchSink):
         Args:
             context: Stream partition or context dictionary.
         """
-        # Sample:
-        # ------
-        # client.upload(context["file_path"])  # Upload file
-        # Path(context["file_path"]).unlink()  # Delete local copy
+        now_ts = datetime.datetime.now().timestamp()
+        records = context["records"]
+        import_name = f"target-hubspot-{self.stream_name}-{now_ts}"
+        csv_filename = f"{import_name}.csv"
+        try:
+            # TODO: do records need to be deterministically sorted so mapping lines up?
+            self._write_csv(csv_filename, records)
+            self._validate_column_mapping(records)
+            request_kwargs = self._build_request_kwargs(csv_filename, import_name)
+            completion_metadata = self._import_csv_and_poll_for_status(request_kwargs)
+            self._validate_completion_metadata(completion_metadata)
+        except Exception as ex:
+            self.logger.error(f"Exception raised while submitting job to HubSpot: {ex}")
+            raise ex
+        finally:
+            os.remove(csv_filename)
+
+    def _parse_timestamps_in_record(
+        self,
+        record: dict,
+        schema: dict,
+        treatment: DatetimeErrorTreatmentEnum,
+    ) -> None:
+        """Parse strings to datetime.datetime values, repairing or erroring on failure.
+
+        Attempts to parse every field that is of type date/datetime/time. If its value
+        is out of range, repair logic will be driven by the `treatment` input arg:
+        MAX, NULL, or ERROR.
+
+        Args:
+            record: Individual record in the stream.
+            schema: TODO
+            treatment: TODO
+        """
+        for key in record:
+            datelike_type = get_datelike_property_type(schema["properties"][key])
+            if datelike_type:
+                date_val = record[key]
+                try:
+                    if record[key] is not None:
+                        date_val = parser.parse(date_val)
+                        # This forces all datelike types to dates, which is what the HubSpot API expects
+                        date_val = date_val.date()
+                        # TODO: add to SDK so date is returned instead of datetime
+                        # if datelike_type == "date":
+                        #     date_val = date_val.date()
+                except parser.ParserError as ex:
+                    date_val = handle_invalid_timestamp_in_record(
+                        record,
+                        [key],
+                        date_val,
+                        datelike_type,
+                        ex,
+                        treatment,
+                        self.logger,
+                    )
+                record[key] = date_val
